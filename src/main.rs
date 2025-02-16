97612d7c392f8d34ce9f9b4afe9fd7479f92f603
Ogi-Git <ognjen25aleksic@gmail.com>
2025-02-16 01:42:02 +0100

    Remove Non-IPv4

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ fn main() -> io::Result<()> {
         let  nbytes = nic.recv(&mut buf[..])?;
         let flags = u16::from_be_bytes([buf[0], buf[1]]);
         let proto = u16::from_be_bytes([buf[2], buf[3]]);
+        if proto != 0x800 {
+            //not IPv4
+            continue;
+        }
         eprintln!("Read {}, flags: {:x}, proto: {:x}, bytes: {:X?}", nbytes - 4, flags, proto, hex::encode(&buf[..nbytes]));
     };
 
