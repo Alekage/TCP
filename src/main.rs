53b13ed0192c9114c0f16a71cf319c05b4d68f31
Ogi-Git <ognjen25aleksic@gmail.com>
2025-02-17 18:05:52 +0100

    Add Ip header slicing

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,25 @@ fn main() -> io::Result<()> {
     let mut buf = [0u8; 1504];
     
     loop {
-        let  nbytes = nic.recv(&mut buf[..])?;
-        let flags = u16::from_be_bytes([buf[0], buf[1]]);
-        let proto = u16::from_be_bytes([buf[2], buf[3]]);
-        if proto != 0x800 {
+        let nbytes = nic.recv(&mut buf[..])?;
+        let _flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
+        if eth_proto != 0x800 {
             //not IPv4
             continue;
         }
-        eprintln!("Read {}, flags: {:x}, proto: {:x}, bytes: {:X?}", nbytes - 4, flags, proto, hex::encode(&buf[..nbytes]));
-    };
 
-    Ok(())
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+            Err(e) => {
+                eprintln!("Ignoring packet: {:?}", e);
+            },
+            Ok(p) => {
+                let src = p.source();
+                let dst = p.destination_addr();
+                let proto = p.protocol();
+                println!("{:?} -> {} {:?}b of protocol {:?}", src, dst, p.payload_len(), proto);
+            }
+        }
+
+    };
 }
